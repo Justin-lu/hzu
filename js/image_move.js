31ebabae7bc122c29677b22a9b86adf11d17331e
luxiaoyong <gdjyluxiaoyong@gmail.com>
2012-05-19 17:12:56 +0800

    add top

--- js/image_move.js
+++ js/image_move.js
@@ -4,6 +4,7 @@ CS.logo = {
 				var schoolLogo = $('#school-logo')
 				this.logoMoveLeft(csLogo);
 				this.logoMoveRight(schoolLogo);
+        this.backToTop();
 		},
 		logoMoveLeft: function(logo) {
 				logo.animate({
@@ -21,6 +22,29 @@ CS.logo = {
 				700, function() {
 						CS.logo.logoMoveLeft(logo);
 				})
+		},
+		//back-to-top
+		backToTop: function() {
+				$("#back-top").hide();
+				$(function() {
+						$(window).scroll(function() {
+								if ($(this).scrollTop() > 100) {
+										$('#back-top').fadeIn();
+								} else {
+										$('#back-top').fadeOut();
+								}
+						});
+
+						$('#back-top').click(function() {
+								$('body,html').animate({
+										scrollTop: 0
+								},
+								800);
+								return false;
+						});
+				});
 		}
+
 }
 CS.logo.init();
+
